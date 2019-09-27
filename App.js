abd5e9e786819bcc907f8dd70a7fdda699627e06
FatherofDharma <fatherofdharma@gmail.com>
2019-09-27 13:38:26 -0700

    add code to finish Style lesson.

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+//---------------Learn-The-Basics-Lesson----------------------------------------
+//
 // import React, { Component } from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
 // import Bananas from './components/Bananas.js'
@@ -23,7 +25,27 @@
 //   },
 // });
 
-// ---------------------Greeting Example-----------------------------------
+
+//------------Props-Lesson-Example----------------------------------------------
+// import React, { Component } from 'react';
+// import { AppRegistry, Image, View } from 'react-native';
+//
+// export default class Bananas extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
+//     return (
+//       <View>
+//         <Image source={pic} style={{width: 193, height: 110}}/>
+//       </View>
+//     );
+//   }
+// }
+
+// AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+
+// -------------Props-Lesson-Greeting Example-----------------------------------
 // import React, { Component } from 'react';
 // import { AppRegistry, Text, View } from 'react-native';
 //
@@ -51,99 +73,121 @@
 //
 // AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
 
-//--------------Image Example-----------------------------------------------
+
+//-----------------State Example------------------------------------------------
+
 // import React, { Component } from 'react';
-// import { AppRegistry, Image, View } from 'react-native';
+// import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 //
-// export default class Bananas extends Component {
+// class Blink extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isShowingText: true };
+//
+//     // Toggle the state every second
+//     setInterval(() => (
+//       this.setState(previousState => (
+//         { isShowingText: !previousState.isShowingText }
+//       ))
+//     ), 1000);
+//   }
+//
 //   render() {
-//     let pic = {
-//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-//     };
+//     if (!this.state.isShowingText) {
+//       return null;
+//     }
+//
 //     return (
-//       <View>
-//         <Image source={pic} style={{width: 193, height: 110}}/>
+//       <Text>{this.props.text}</Text>
+//     );
+//   }
+// }
+//
+// class Blink2 extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isShowingText: true };
+//
+//     // Toggle the state every second
+//     setInterval(() => (
+//       this.setState(previousState => (
+//         { isShowingText: !previousState.isShowingText }
+//       ))
+//     ), 2000);
+//   }
+//
+//   render() {
+//     if (!this.state.isShowingText) {
+//       return null;
+//     }
+//
+//     return (
+//       <Text>{this.props.text}</Text>
+//     );
+//   }
+// }
+//
+// export default class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container} >
+//         <Blink text='I love to blink' />
+//         <Blink2 text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' />
 //       </View>
 //     );
 //   }
 // }
 
-// AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-//-----------------State Example-------------------------------------------
+// skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
+
+//-------------------Styles Example---------------------------------------------
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isShowingText: true };
-
-    // Toggle the state every second
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  container: {
+    flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
   }
+});
 
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-
-class Blink2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isShowingText: true };
-
-    // Toggle the state every second
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 2000);
-  }
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
   render() {
     return (
-      <View style={styles.container} >
-        <Blink text='I love to blink' />
-        <Blink2 text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+      <View style={styles.container}>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+
+//-------------------Height-and-Width-Lesson------------------------------------
