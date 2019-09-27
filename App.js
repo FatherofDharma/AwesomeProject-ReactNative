eb0a94b0f9b4614f8cc48e31249fee299cf8a247
FatherofDharma <fatherofdharma@gmail.com>
2019-09-27 15:52:16 -0700

    add some code from handling touches lesson.

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-//---------------Learn-The-Basics-Lesson----------------------------------------
+// ---------------Learn-The-Basics-Lesson----------------------------------------
 //
 // import React, { Component } from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
-// import Bananas from './components/Bananas.js'
-//
 //
 // export default class HelloWorldApp extends Component {
 //   render() {
@@ -11,7 +9,6 @@
 //       <View style={styles.container}>
 //         <Text>Hello world, I am here!</Text>
 //       </View>
-//       // <Bananas />
 //     );
 //   }
 // }
@@ -24,7 +21,7 @@
 //     justifyContent: 'center',
 //   },
 // });
-
+//
 
 //------------Props-Lesson-Example----------------------------------------------
 // import React, { Component } from 'react';
@@ -308,32 +305,194 @@
 
 //-------------------------Handling-Text-Input-Lesson---------------------------
 
-import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
-
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
-  render() {
-    return (
-      <View style={{padding: 10, alignItems: 'center'}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-      </View>
-    );
-  }
-}
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, TextInput, View } from 'react-native';
+//
+// export default class PizzaTranslator extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {text: ''};
+//   }
+//
+//   render() {
+//     return (
+//       <View style={{padding: 10, alignItems: 'center'}}>
+//         <TextInput
+//           style={{height: 40}}
+//           placeholder="Type here to translate!"
+//           onChangeText={(text) => this.setState({text})}
+//         />
+//         <Text style={{padding: 10, fontSize: 42}}>
+//           {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
 
 //---------------------------Handling-Touches-Lesson----------------------------
+
+
+//-----------------Handling-Touches-Lesson-Touchable-Highlight------------------
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   TouchableHighlight,
+//   Text,
+//   View,
+// } from 'react-native';
+//
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { count: 0 }
+//   }
+//
+//   onPress = () => {
+//     this.setState({
+//       count: this.state.count+1
+//     })
+//   }
+//
+//  render() {
+//     return (
+//       <View style={styles.container}>
+//         <TouchableHighlight
+//          style={styles.button}
+//          onPress={this.onPress}
+//         >
+//          <Text> Touch Here </Text>
+//         </TouchableHighlight>
+//         <View style={[styles.countContainer]}>
+//           <Text style={[styles.countText]}>
+//             { this.state.count !== 0 ? this.state.count: null}
+//           </Text>
+//         </View>
+//       </View>
+//     )
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     paddingHorizontal: 10
+//   },
+//   button: {
+//     alignItems: 'center',
+//     backgroundColor: '#DDDDDD',
+//     padding: 10
+//   },
+//   countContainer: {
+//     alignItems: 'center',
+//     padding: 10
+//   },
+//   countText: {
+//     color: '#FF00FF'
+//   }
+// })
+//
+// AppRegistry.registerComponent('AwesomeProject', () => App)
+//
+//---------------------Button-Example-From-Different-Section--------------------
+
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+import Constants from 'expo-constants';
+
+function Separator() {
+  return <View style={styles.separator} />;
+}
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          The title and onPress handler are required. It is recommended to set
+          accessibilityLabel to help make your app usable by everyone.
+        </Text>
+        <Button
+          title="Press me"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          Adjust the color in a way that looks standard on each platform. On
+          iOS, the color prop controls the color of the text. On Android, the
+          color adjusts the backgroud color of the button.
+        </Text>
+        <Button
+          title="Press me"
+          color="#f194ff"
+          onPress={() => Alert.alert('Button with adjusted color pressed')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          All interaction for the component are disabled.
+        </Text>
+        <Button
+          title="Press me"
+          disabled
+          onPress={() => Alert.alert('Cannot press this one')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          This layout strategy lets the title define the width of the button.
+        </Text>
+        <View style={styles.fixToText}>
+          <Button
+            title="Left button"
+            onPress={() => Alert.alert('Left button pressed')}
+          />
+          <Button
+            title="Right button"
+            onPress={() => Alert.alert('Right button pressed')}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => App)
