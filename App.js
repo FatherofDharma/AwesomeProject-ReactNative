728dcebdf16944b06b0ca3b991e38a0985967b12
FatherofDharma <fatherofdharma@gmail.com>
2019-09-27 11:45:06 -0700

    finish Props lesson of the React-Native tutorial

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import React, { Component } from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Bananas from './components/Bananas.js'
+//
+//
+// export default class HelloWorldApp extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Hello world, I am here!</Text>
+//       </View>
+//       // <Bananas />
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-export default class HelloWorldApp extends Component {
+// ---------------------Greeting Example-----------------------------------
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
+
+//--------------Image Example-----------------------------------------------
+// import React, { Component } from 'react';
+// import { AppRegistry, Image, View } from 'react-native';
+//
+// export default class Bananas extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
+//     return (
+//       <View>
+//         <Image source={pic} style={{width: 193, height: 110}}/>
+//       </View>
+//     );
+//   }
+// }
+
+// AppRegistry.registerComponent('AwesomeProject', () => Bananas);
